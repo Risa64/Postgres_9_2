3bf311074630d8baa1f33a5a73e7e95d57a86ac3
Risa64 <happyworkday64@gmail.com>
2026-09-25 15:11:53 +0900

    connected

--- src/postgres/DatabaseSample_01_Products_01.java
+++ src/postgres/DatabaseSample_01_Products_01.java
@@ -6,7 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseSample_01_Products_01 {
+// PostgreSQLのResultSetの実装クラスでサポートされてない者たち
+// Java 的には便利だろうと思って作ったけど、サポートされてないので結局使えない子たち以下三つ
+// resultSet.absolute(5);
+// resultSet.first();
+// resultSet.deleteRow();
+
+class DatabaseSample_01_Products_01 {
 	public static void main(String[] args) {
 		// JDBC ドライバでPosteSQLサーバーに接続
 		
@@ -20,8 +26,8 @@ public class DatabaseSample_01_Products_01 {
 		// 接続が成功すると、connectionのインスタンスを返してくれる。ことにより、SQL
 		
 		Statement statement = connection.createStatement();
-		// Postgres用のインスタンスを返してくれる
-		// SQLを実行するためのstatementインスタンス
+		// Postgres用のインスタンスを返してくれる←？
+		// SQLを実行するためのstatementインスタンス←？
 		
 	
 //		String sql = "select * from products order by product_id asc;";
@@ -31,14 +37,20 @@ public class DatabaseSample_01_Products_01 {
 		while (resultSet.next()) {
 		    // 各カラムを取り出す
 //			System.out.println(resultSet); // like hash values
-			System.out.println(resultSet.getString("product_id"));
+			String productId = resultSet.getString("product_id");
+			System.out.println("Product ID : " + productId);
+			
 			System.out.println(resultSet.getString("product_name"));
 			System.out.println(resultSet.getString("product_type"));
 			System.out.println(resultSet.getString("price"));
 			System.out.println(resultSet.getString("cost"));
 		}
 		
-		
+		// Do not forget close!
+		resultSet.close();
+		statement.close();
+		// cutout connection
+		connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
